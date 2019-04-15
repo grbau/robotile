85fd83e443b836f09126b2a58b7dc44cd1a11f05
gregory <gregory.baudic@gmail.com>
2019-04-15 22:44:24 +0200

    contact form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: '/home',
     pathMatch: 'full',
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   },
 ];
 
